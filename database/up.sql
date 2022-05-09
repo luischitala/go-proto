262edd9be52917504c820786c2450fa6b16269ab
luischitala <lrchitala@gmail.com>
2022-05-08 23:17:15 -0500

    Course started

---
+++ database/up.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students(
+    id VARCHAR(32) PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    age INTEGER NOT NULL
+)
---
+++ repository/repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/luischita/go/grpc/models"
+)
+
+type Repository interface {
+	GetStudent(ctx context.Context, id string) (*models.Student, error)
+	SetStudent(ctx context.Context, student *models.Student) error
+}
+
+//Create implementation
+
+var implementation Repository
+
+//Dependency injection
+func SetRepository(repository Repository) {
+	implementation = repository
+}
+
+func SetStudent(ctx context.Context, student *models.Student) error {
+	return implementation.SetStudent(ctx, student)
+}
+
+func GetStudent(ctx context.Context, id string) (*models.Student, error) {
+	return implementation.GetStudent(ctx, id)
+}
